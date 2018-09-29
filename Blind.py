cf6bc1e5409124f3feedfce1f254635e2dd11061
baber-sos <baber.khalid@rutgers.edu>
2018-09-28 23:25:10 -0400

    bfs/dfs working

--- Blind.py
+++ Blind.py
@@ -29,7 +29,14 @@ MAZE = [[0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
 
 
-def plot(maze, GRIDSIZE):
+def plot_v2(maze, name, path, visited):
+    print path[0];
+    directions = {'Down': (1, 0),
+            'Up': (-1, 0),
+            'Right': (0, 1),
+            'Left': (0, -1)};
+    print directions;
+    GRIDSIZE = (len(maze[0]), len(maze));
     plt.xlim(0, GRIDSIZE[1]);
     plt.ylim(0, GRIDSIZE[0]);
     ax = plt.axes();
@@ -40,11 +47,18 @@ def plot(maze, GRIDSIZE):
         for j in xrange(GRIDSIZE[1]):
             if maze[i][j] == 1:
                 plt.plot( j + 0.5, GRIDSIZE[0] - i - 0.5, 'o', color='black');
-            elif maze[i][j] == -1:
-                plt.plot( j + 0.5, GRIDSIZE[0] - i - 0.5, 'x', color='blue');
-            elif maze[i][j] == 2:
-                plt.plot( j + 0.5, GRIDSIZE[0] - i - 0.5, 'o', color='red');
-    plt.savefig('Astar_Euclidean.png');
+    start = [0, 0];
+    if path == []:
+        plt.savefig(name);
+        return;
+    for coord in visited:
+        plt.plot( coord[1] + 0.5, GRIDSIZE[0] - coord[0] - 0.5, 'x', color='blue');
+    for dir in path:
+        print 'cuurent dir: ', dir;
+        plt.plot( start[1] + 0.5, GRIDSIZE[0] - start[0] - 0.5, 'o', color='red');
+        start[0] += directions[dir][0]; start[1] += directions[dir][1]; 
+    plt.plot( start[1] + 0.5, GRIDSIZE[0] - start[0] - 0.5, 'o', color='red');
+    plt.savefig(name);
 
 # graph represented as dict; keys = node coord, values = neighbor coord (tuple with 2 num)
 def convertMaze(maze):
@@ -53,18 +67,18 @@ def convertMaze(maze):
     graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
     for row, col in graph.keys():
         if row < height - 1 and not maze[row + 1][col]:
-            graph[(row, col)].append(("Down, ", (row + 1, col)))
-            graph[(row + 1, col)].append(("Up, ", (row, col)))
+            graph[(row, col)].append(("Down", (row + 1, col)))
+            graph[(row + 1, col)].append(("Up", (row, col)))
         if col < width - 1 and not maze[row][col + 1]:
-            graph[(row, col)].append(("Right, ", (row, col + 1)))
-            graph[(row, col + 1)].append(("Left, ", (row, col)))
+            graph[(row, col)].append(("Right", (row, col + 1)))
+            graph[(row, col + 1)].append(("Left", (row, col)))
     return graph
 
 
 # queue of initial node, check if first in queue is visited, if next is goal, add neighbor to queue
 def solveBFS(maze):
     start, goal = (0, 0), (len(maze) - 1, len(maze[0]) - 1)
-    queue = deque([("", start)])
+    queue = deque([([], start)])
     visited = set()
     graph = convertMaze(maze)
     explored = 0;
@@ -77,14 +91,14 @@ def solveBFS(maze):
         visited.add(current)
         explored += 1;
         for direction, neighbour in graph[current]:
-            queue.append((path + direction, neighbour))
+            queue.append((path + [direction], neighbour))
     return explored, [], visited;
 
 
 # bfs with stack
 def solveDFS(maze):
     start, goal = (0, 0), (len(maze) - 1, len(maze[0]) - 1)
-    stack = deque([("", start)])
+    stack = deque([([], start)])
     visited = set()
     graph = convertMaze(maze)
     explored = 0;
@@ -97,7 +111,7 @@ def solveDFS(maze):
         visited.add(current)
         explored += 1
         for direction, neighbour in graph[current]:
-            stack.append((path + direction, neighbour))
+            stack.append((path + [direction], neighbour))
     return explored, [], visited;
 
 
@@ -105,9 +119,12 @@ if __name__ == "__main__":
 
     #start = time.time();
     print("BFS Path:")
-    print(solveBFS(MAZE))
+    explored_number, path, visited = solveBFS(MAZE);
+    print path
+    plot_v2(MAZE, 'bfs.png', path, visited);
     print("DFS Path:")
-    print(solveDFS(MAZE))
+    explored_number, path, visited = solveDFS(MAZE);
+    plot_v2(MAZE, 'dfs.png', path, visited);
     #end = time.time();
     #print(end - start);
 
