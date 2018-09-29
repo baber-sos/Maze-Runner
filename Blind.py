3bf54feb7a0803e9a387d4c8f26d16d96ded6fab
Kyung-Lee <omgchickenz@gmail.com>
2018-09-28 22:52:31 -0400

    updated # nodes explored/visited nodes

--- Blind.py
+++ Blind.py
@@ -1,15 +1,58 @@
 import os
 import time
+import random
+import heapq
+from math import sqrt
+import matplotlib.pyplot as plt
 from collections import deque
 
 
-# graph of dictionaries where key = current coord, values = neighbor coord; append directions per access
+GRIDSIZE = (10,10);
+MAZE = [[0, 1, 0, 1, 0, 0, 0, 0, 0, 0],
+
+        [0, 0, 1, 1, 0, 0, 0, 0, 0, 0],
+
+        [0, 0, 1, 0, 0, 0, 1, 0, 0, 0],
+
+        [1, 0, 1, 0, 0, 0, 0, 0, 0, 0],
+
+        [0, 0, 1, 0, 0, 0, 1, 0, 1, 1],
+
+        [1, 0, 0, 1, 0, 0, 0, 1, 0, 0],
+
+        [0, 0, 1, 0, 0, 0, 1, 1, 0, 1],
+
+        [0, 0, 0, 0, 0, 0, 1, 0, 1, 0],
+
+        [0, 0, 1, 0, 0, 1, 0, 0, 0, 0],
+
+        [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+
+def plot(maze, GRIDSIZE):
+    plt.xlim(0, GRIDSIZE[1]);
+    plt.ylim(0, GRIDSIZE[0]);
+    ax = plt.axes();
+    ax.set_xticks( [i for i in xrange(GRIDSIZE[1])], minor=False);
+    ax.set_yticks( [i for i in xrange(GRIDSIZE[0])], minor=False);
+    plt.grid(True);
+    for i in xrange(GRIDSIZE[0]):
+        for j in xrange(GRIDSIZE[1]):
+            if maze[i][j] == 1:
+                plt.plot( j + 0.5, GRIDSIZE[0] - i - 0.5, 'o', color='black');
+            elif maze[i][j] == -1:
+                plt.plot( j + 0.5, GRIDSIZE[0] - i - 0.5, 'x', color='blue');
+            elif maze[i][j] == 2:
+                plt.plot( j + 0.5, GRIDSIZE[0] - i - 0.5, 'o', color='red');
+    plt.savefig('Astar_Euclidean.png');
+
+# graph represented as dict; keys = node coord, values = neighbor coord (tuple with 2 num)
 def convertMaze(maze):
-    length = len(maze)
-    width = len(maze[0]) if length else 0
-    graph = {(i, j): [] for j in range(width) for i in range(length) if not maze[i][j]}
+    height = len(maze)
+    width = len(maze[0]) if height else 0
+    graph = {(i, j): [] for j in range(width) for i in range(height) if not maze[i][j]}
     for row, col in graph.keys():
-        if row < length - 1 and not maze[row + 1][col]:
+        if row < height - 1 and not maze[row + 1][col]:
             graph[(row, col)].append(("Down, ", (row + 1, col)))
             graph[(row + 1, col)].append(("Up, ", (row, col)))
         if col < width - 1 and not maze[row][col + 1]:
@@ -18,75 +61,54 @@ def convertMaze(maze):
     return graph
 
 
-# queue of initial node -> check if next node is goal node, if not add neighbor nodes to queue and repeat
+# queue of initial node, check if first in queue is visited, if next is goal, add neighbor to queue
 def solveBFS(maze):
-    s, g = (0, 0), (len(maze) - 1, len(maze[0]) - 1)
-    queue = deque([("", s)])
+    start, goal = (0, 0), (len(maze) - 1, len(maze[0]) - 1)
+    queue = deque([("", start)])
     visited = set()
     graph = convertMaze(maze)
+    explored = 0;
     while queue:
         path, current = queue.popleft()
-        if current == g:
-            return path
+        if current == goal:
+            return explored, path, visited
         if current in visited:
             continue
         visited.add(current)
+        explored += 1;
         for direction, neighbour in graph[current]:
             queue.append((path + direction, neighbour))
-    return "No solvable path"
+    return explored, [], visited;
 
 
-# bfs but stack
+# bfs with stack
 def solveDFS(maze):
     start, goal = (0, 0), (len(maze) - 1, len(maze[0]) - 1)
     stack = deque([("", start)])
     visited = set()
     graph = convertMaze(maze)
+    explored = 0;
     while stack:
         path, current = stack.pop()
         if current == goal:
-            return path
+            return explored, path, visited;
         if current in visited:
             continue
         visited.add(current)
+        explored += 1
         for direction, neighbour in graph[current]:
             stack.append((path + direction, neighbour))
-    return "No solvable path"
-
-
-MAZE = [
-    [0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1],
-    [1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 1, 0, 0, 0, 0, 0, 0, 1],
-    [1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1],
-    [1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1],
-    [1, 0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 1],
-    [1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
-    [1, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1],
-    [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0],
-]
+    return explored, [], visited;
 
 
 if __name__ == "__main__":
-
-    #MAZE.find_path_bfs;
-    start = time.time();
-    #find_path_bfs(MAZE)
-
 
     #start = time.time();
     print("BFS Path:")
     print(solveBFS(MAZE))
     print("DFS Path:")
     print(solveDFS(MAZE))
-    end = time.time();
-    print(end - start);
+    #end = time.time();
+    #print(end - start);
 
     print("----------------------------")
-
-# num of explored nodes
-# path taken
-# maze generated
